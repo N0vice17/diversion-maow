c79eefc9deb91bfd7f1446f419895b5654cccd20
cu8code <git8anaknroy@gmai.com>
2024-01-22 23:22:04 +0530

    pan verify

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,106 +1,99 @@
-import { ConnectWallet } from "@thirdweb-dev/react";
-import styles from "../styles/Home.module.css";
-import Image from "next/image";
+// import styles from "../styles/Home.module.css";
+
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+
+function Card({ children, className }) {
+  return (
+    <div className={`${className} drop-shadow`}>{children}</div>
+  )
+}
+
+function Input({ className, label, type }) {
+  return (
+    <div className={className}>
+      <p>{label} <a className="text-red-900">*</a></p>
+      <input title="ankan" type={type} className="rounded w-full text-xl p-2 border-red-700 border" />
+    </div>
+  )
+}
+
+function Navigator({ id }) {
+  return (
+    <div className="flex gap-10">
+      <div>
+        <div id="1" className="rounded font-bold flex border-blue-700 border-2 bg-white max-h-8 justify-center items-center max-w-8 p-1">1</div>
+        <div>Enter Details</div>
+      </div>
+      <div>
+        <div id="2" className="flex border-gray-300  border-2 bg-gray-100 text-gray-400 max-h-8 justify-center items-center max-w-8 p-1">2</div>
+        <div className="text-gray-400">Verification</div>
+      </div>
+      <div>
+        <div id="2" className="flex border-gray-300  border-2 bg-gray-100 text-gray-400 max-h-8 justify-center items-center max-w-8 p-1">3</div>
+        <div className="text-gray-400">Vote</div>
+      </div>
+    </div>
+  )
+}
+
+function NavBar() {
+  return (
+    <>
+      <nav className="z-1 relative h-10 bg-white"></nav>
+      <div className="z-1 relative h-5 bg-blue-700 shadow shadow-neutral-300"></div>
+    </>
+  )
+}
+
+function Footer() {
+  return (
+    <footer className="z-1 flex relative bg-blue-900 h-full w-full">
+    </footer>
+  )
+}
 
 export default function Home() {
+  const [adharNumber, setAdharNumber] = useState("")
+  const [voterId, setVoterId] = useState("")
+  const router = useRouter()
+  useEffect(() => {
+    if ("111" === adharNumber &&
+      "111" === voterId) {
+      router.push("vote")
+    }
+  }, [adharNumber, voterId])
   return (
-    <main className={styles.main}>
-      <div className={styles.container}>
-        <div className={styles.header}>
-          <h1 className={styles.title}>
-            Welcome to{" "}
-            <span className={styles.gradientText0}>
-              <a
-                href="https://thirdweb.com/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                thirdweb.
-              </a>
-            </span>
-          </h1>
-
-          <p className={styles.description}>
-            Get started by configuring your desired network in{" "}
-            <code className={styles.code}>src/index.js</code>, then modify the{" "}
-            <code className={styles.code}>src/App.js</code> file!
-          </p>
-
-          <div className={styles.connect}>
-            <ConnectWallet
-              dropdownPosition={{
-                side: "bottom",
-                align: "center",
-              }}
-            />
+    <>
+      <NavBar />
+      <div className="bg-gray-100 z-1 flex flex-col items-center h-screen">
+        <div className="relative flex flex-col gap-8 m-5 max-w-xl">
+          <Navigator />
+          <div className="flex gap-0.5">
+            <h1 className="text-2xl w-full">Verify Your PAN</h1>
+            <div className="flex w-full justify-end items-end text-gray-600 text-sm gap-0.5">
+              <a className="text-red-900">*</a>
+              Indicates mandatory fields
+            </div>
+          </div>
+          <Card className="rounded flex bg-white border-gray-300 border p-12 gap-10">
+            <div className="flex flex-col basis-28 justify-evenly gap-10">
+              <Input label={"Pan"} type={"tel"} />
+              <Input label={"Bob"} type={"tel"} />
+            </div>
+            <div className="flex flex-col basis-72 justify-evenly gap-10">
+              <Input label={"Full Name"} type={"text"} />
+              <Input label={"Mobile Number"} type={"tel"} />
+            </div>
+          </Card>
+          <div className="flex justify-between p-2">
+            <button className="rounded border-blue-900 border text-blue-900 font-bold p-2">cancel</button>
+            <button className="rounded bg-gray-400 text-gray-200 shadow shadow-gray-500 font-bold p-2">continue</button>
           </div>
         </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://portal.thirdweb.com/"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              src="/images/portal-preview.png"
-              alt="Placeholder preview of starter"
-              width={300}
-              height={200}
-            />
-            <div className={styles.cardText}>
-              <h2 className={styles.gradientText1}>Portal ➜</h2>
-              <p>
-                Guides, references, and resources that will help you build with
-                thirdweb.
-              </p>
-            </div>
-          </a>
-
-          <a
-            href="https://thirdweb.com/dashboard"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              src="/images/dashboard-preview.png"
-              alt="Placeholder preview of starter"
-              width={300}
-              height={200}
-            />
-            <div className={styles.cardText}>
-              <h2 className={styles.gradientText2}>Dashboard ➜</h2>
-              <p>
-                Deploy, configure, and manage your smart contracts from the
-                dashboard.
-              </p>
-            </div>
-          </a>
-
-          <a
-            href="https://thirdweb.com/templates"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              src="/images/templates-preview.png"
-              alt="Placeholder preview of templates"
-              width={300}
-              height={200}
-            />
-            <div className={styles.cardText}>
-              <h2 className={styles.gradientText3}>Templates ➜</h2>
-              <p>
-                Discover and clone template projects showcasing thirdweb
-                features.
-              </p>
-            </div>
-          </a>
-        </div>
+      <Footer />
       </div>
-    </main>
+    </>
   );
 }
+
---
+++ frontend/pages/vote/index.js
@@ -0,0 +1,14 @@
+export default function Vote() {
+  return (
+    <>
+      <div className="z-0 bg w-screen h-screen absolute top-0 left-0"></div>
+      <div className="z-1 relative flex flex-col justify-center items-center w-screen h-screen">
+        <div className="outline-blue p-1">
+          <h1>Vote</h1>
+          <div className="flex gap-1">
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
