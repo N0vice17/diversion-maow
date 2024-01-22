13501b02bac68af044581a620025ad86cde0abf1
cu8code <git8anaknroy@gmai.com>
2024-01-23 01:15:15 +0530

    added connect wallet step

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -13,7 +13,7 @@ function Input({ className, label, type }) {
   return (
     <div className={className}>
       <p>{label} <a className="text-red-900">*</a></p>
-      <input title="ankan" type={type} className="rounded w-full text-xl p-2 border-red-700 border" />
+      <input title="ankan" type={type} className="rounded w-full text-md p-2 border-red-700 border" />
     </div>
   )
 }
@@ -21,8 +21,8 @@ function Input({ className, label, type }) {
 function Navigator({ id }) {
   return (
     <div className="flex gap-10">
-      <div>
-        <div id="1" className="rounded font-bold flex border-blue-700 border-2 bg-white max-h-8 justify-center items-center max-w-8 p-1">1</div>
+      <div id="1">
+        <div id="t" className="rounded font-bold flex border-blue-700 border-2 bg-white max-h-8 justify-center items-center max-w-8 p-1">1</div>
         <div>Enter Details</div>
       </div>
       <div>
@@ -31,6 +31,10 @@ function Navigator({ id }) {
       </div>
       <div>
         <div id="2" className="flex border-gray-300  border-2 bg-gray-100 text-gray-400 max-h-8 justify-center items-center max-w-8 p-1">3</div>
+        <div className="text-gray-400">connect wallet</div>
+      </div>
+      <div>
+        <div id="2" className="flex border-gray-300  border-2 bg-gray-100 text-gray-400 max-h-8 justify-center items-center max-w-8 p-1">4</div>
         <div className="text-gray-400">Vote</div>
       </div>
     </div>
@@ -40,15 +44,15 @@ function Navigator({ id }) {
 function NavBar() {
   return (
     <>
-      <nav className="z-1 relative h-10 bg-white"></nav>
-      <div className="z-1 relative h-5 bg-blue-700 shadow shadow-neutral-300"></div>
+      <nav className="h-10 bg-white"></nav>
+      <div className="h-5 bg-blue-700 shadow shadow-neutral-300"></div>
     </>
   )
 }
 
 function Footer() {
   return (
-    <footer className="z-1 flex relative bg-blue-900 h-full w-full">
+    <footer className="flex relative bg-blue-900 h-full w-full">
     </footer>
   )
 }
@@ -66,7 +70,7 @@ export default function Home() {
   return (
     <>
       <NavBar />
-      <div className="bg-gray-100 z-1 flex flex-col items-center h-screen">
+      <div className="bg-gray-100 flex flex-col items-center h-screen">
         <div className="relative flex flex-col gap-8 m-5 max-w-xl">
           <Navigator />
           <div className="flex gap-0.5">
@@ -79,7 +83,7 @@ export default function Home() {
           <Card className="rounded flex bg-white border-gray-300 border p-12 gap-10">
             <div className="flex flex-col basis-28 justify-evenly gap-10">
               <Input label={"Pan"} type={"tel"} />
-              <Input label={"Bob"} type={"tel"} />
+              <Input label={"Bob"} type={"date"} />
             </div>
             <div className="flex flex-col basis-72 justify-evenly gap-10">
               <Input label={"Full Name"} type={"text"} />
@@ -91,7 +95,7 @@ export default function Home() {
             <button className="rounded bg-gray-400 text-gray-200 shadow shadow-gray-500 font-bold p-2">continue</button>
           </div>
         </div>
-      <Footer />
+        <Footer />
       </div>
     </>
   );
